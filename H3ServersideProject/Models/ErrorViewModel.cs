5a0f2cb16bf32cd3171d6594888ccf489d260d04
MadsRasmussenStofo <mra@stofo.dk>
2023-02-22 10:46:12 +0100

    Igangværende projekt

--- H3ServersideProject/Models/ErrorViewModel.cs
+++ H3ServersideProject/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace H3ServersideProject.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
+         
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
